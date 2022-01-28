a054a48327800e7b87f4b7fd6cb3d9e5f45ec020
Daniel Zamudio <daniel.zamudio@ymail.com>
2022-01-28 10:43:52 -0600

    Updated course

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+/**
+ * NEED TO COMPLETE VUE 2019 CRASH COURSE!
+ */
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+ router,
+ render: h => h(App)
 }).$mount('#app')
---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0. It is part of the Vue Crash Course on YouTube</p>
+    <input type="checkbox" name="Netlify Deployment" id="last-step"> <a href='https://rb.gy/vqiaq4' target='_blank'>Netlify Deployment</a>
+  </div>
+</template>
